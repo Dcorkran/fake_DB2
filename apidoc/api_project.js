c450a77cc20c667761a559d05a0a4ec6abc8867a
Dillon Corkran <dcorkran972@gmail.com>
2017-01-26 10:35:59 -0700

    :sparkles: apidoc website added

---
+++ apidoc/api_project.js
@@ -0,0 +1,1 @@
+define({  "name": "MonsterTruck API",  "version": "1.0.0",  "description": "MonsterTruck JSON API",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2017-01-26T17:31:47.512Z",    "url": "http://apidocjs.com",    "version": "0.17.5"  }});
